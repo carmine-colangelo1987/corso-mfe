448da953a0023e0545cc8b3d87a9c4abeb295a8e
carmine_colangelo <carmine.colangelo2@alten.it>
2023-09-08 12:32:06 +0200

    css in js

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {Switch, Route, BrowserRouter} from "react-router-dom";
-import {StylesProvider} from "@material-ui/core/styles";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+/**
+ * Questa funzione genera un prefisso diverso alle classi custom generate tramite la libreria "createStyle"
+ *
+ * In produzione le classi CSS avranno un prefisso diverso da quello di default.
+ * Questo serve a prevenire l'ereditarietà del CSS tra i vari microfrontend (nulla va dato per scontato)
+ */
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+})
+
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing} />
